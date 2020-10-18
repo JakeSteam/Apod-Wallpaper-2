3ad7609d78e6f4c5ec21fb9d789ebc297be2ac56
Jake Lee <github@jakelee.co.uk>
2020-10-18 19:12:10 +0100

    Adds deep links and universal links

--- app/src/main/java/uk/co/jakelee/apodwallpaper/MainActivity.kt
+++ app/src/main/java/uk/co/jakelee/apodwallpaper/MainActivity.kt
@@ -1,11 +1,11 @@
 package uk.co.jakelee.apodwallpaper
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -25,5 +25,27 @@ class MainActivity : AppCompatActivity() {
     ))
     setupActionBarWithNavController(navController, appBarConfiguration)*/
     navView.setupWithNavController(navController)
+
+    tryNavigateDeepLink(navController, intent)
+  }
+
+  override fun onNewIntent(intent: Intent?) {
+    super.onNewIntent(intent)
+    val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+    tryNavigateDeepLink(navHostFragment.navController, intent)
+  }
+
+  private fun tryNavigateDeepLink(navController: NavController, intent: Intent?) {
+    if (intent == null || intent.action != Intent.ACTION_VIEW || intent.data == null) return
+    val data = intent.data!!
+
+    val action = if (data.scheme == "apod") data.host else data.pathSegments.getOrNull(0)
+    val parameter = if (data.scheme == "apod") data.pathSegments.getOrNull(0) else data.pathSegments.getOrNull(1)
+    when (action) {
+      "browse" -> navController.navigate(R.id.navigation_browse)
+      "settings" -> navController.navigate(R.id.navigation_settings)
+      "more" -> navController.navigate(R.id.navigation_more)
+      "day" -> navController.navigate(NavigationDirections.openApod(null, parameter))
+    }
   }
 }
