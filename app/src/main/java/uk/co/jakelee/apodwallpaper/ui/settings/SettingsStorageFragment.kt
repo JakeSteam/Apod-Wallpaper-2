8d04c69860c8196f7d5c708849d17e9f8552c800
Jake Lee <github@jakelee.co.uk>
2020-11-09 20:31:07 +0000

    Adds displaying current image cache size

--- app/src/main/java/uk/co/jakelee/apodwallpaper/ui/settings/SettingsStorageFragment.kt
+++ app/src/main/java/uk/co/jakelee/apodwallpaper/ui/settings/SettingsStorageFragment.kt
@@ -1,18 +1,22 @@
 package uk.co.jakelee.apodwallpaper.ui.settings
 
 import android.os.Bundle
+import android.text.format.Formatter
 import androidx.lifecycle.lifecycleScope
 import androidx.preference.*
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.cache.DiskCache
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import uk.co.jakelee.apodwallpaper.R
+import uk.co.jakelee.apodwallpaper.extensions.getFolderInfo
+import java.io.File
 
 class SettingsStorageFragment : SettingsBaseFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        updateImageCacheSize("initial")
+        updateImageCacheSize()
     }
 
     override val preferencesFile = R.xml.preferences_storage
@@ -33,13 +37,14 @@ class SettingsStorageFragment : SettingsBaseFragment() {
 
     private fun clearImageCache() = lifecycleScope.launch(Dispatchers.IO) {
         Glide.get(requireActivity().applicationContext).clearDiskCache()
-        updateImageCacheSize("done")
+        updateImageCacheSize()
     }
 
-    private fun updateImageCacheSize(size: String) {
-        lifecycleScope.launch {
+    private fun updateImageCacheSize() {
+        val cacheInfo = File(requireContext().cacheDir, DiskCache.Factory.DEFAULT_DISK_CACHE_DIR).getFolderInfo()
+        lifecycleScope.launch(Dispatchers.Main) {
             val clearCache = findPreference<Preference>(getString(R.string.to_clear_image_cache))
-            clearCache?.summary = size
+            clearCache?.summary = "${Formatter.formatShortFileSize(requireContext(), cacheInfo.bytes)} (${cacheInfo.files} files)"
         }
     }
 }
