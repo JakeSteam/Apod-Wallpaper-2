583beeb0b809c46765cb0a9fc7bb71f8db759dea
Jake Lee <github@jakelee.co.uk>
2020-11-09 20:05:09 +0000

    Adds hacky image cache clearing

--- app/src/main/java/uk/co/jakelee/apodwallpaper/ui/settings/SettingsStorageFragment.kt
+++ app/src/main/java/uk/co/jakelee/apodwallpaper/ui/settings/SettingsStorageFragment.kt
@@ -1,9 +1,19 @@
 package uk.co.jakelee.apodwallpaper.ui.settings
 
+import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
 import androidx.preference.*
+import com.bumptech.glide.Glide
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import uk.co.jakelee.apodwallpaper.R
 
 class SettingsStorageFragment : SettingsBaseFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        updateImageCacheSize("initial")
+    }
 
     override val preferencesFile = R.xml.preferences_storage
 
@@ -15,5 +25,21 @@ class SettingsStorageFragment : SettingsBaseFragment() {
 
     override fun onListPreferenceChanged(pref: ListPreference) {}
 
-    override fun onNavigationPreferenceClicked(pref: Preference) {}
+    override fun onNavigationPreferenceClicked(pref: Preference) {
+        when (pref.key) {
+            getString(R.string.to_clear_image_cache) -> clearImageCache()
+        }
+    }
+
+    private fun clearImageCache() = lifecycleScope.launch(Dispatchers.IO) {
+        Glide.get(requireActivity().applicationContext).clearDiskCache()
+        updateImageCacheSize("done")
+    }
+
+    private fun updateImageCacheSize(size: String) {
+        lifecycleScope.launch {
+            val clearCache = findPreference<Preference>(getString(R.string.to_clear_image_cache))
+            clearCache?.summary = size
+        }
+    }
 }
